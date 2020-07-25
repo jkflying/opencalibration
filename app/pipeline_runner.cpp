5afeecfee163365688fbabb0519aa64b4a77d86a
Julian Kent <jkflying@gmail.com>
2020-07-25 11:36:10 +0200

    Add simple pipeline app to run the pipeline from command line

---
+++ app/pipeline_runner.cpp
@@ -0,0 +1,32 @@
+#include <opencalibration/pipeline/pipeline.hpp>
+
+#include <filesystem>
+#include <thread>
+
+
+using namespace opencalibration;
+using namespace std::chrono_literals;
+
+int main(int argc, char *argv[])
+{
+    Pipeline p(4);
+    std::vector<std::string> files;
+
+    if (argc > 1) {
+        std::string path = argv[1];
+        for (const auto& entry : std::filesystem::directory_iterator(path))
+        {
+            files.push_back(entry.path());
+        }
+    }
+
+    for (const auto& file : files)
+    {
+        p.add(file);
+    }
+
+    while (p.getStatus() != Pipeline::Status::COMPLETE)
+    {
+        std::this_thread::sleep_for(1ms);
+    }
+}
