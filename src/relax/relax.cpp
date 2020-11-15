bb0f41540da7d34c1a8b7d348eef10c038c3361c
Julian Kent <jkflying@gmail.com>
2020-11-15 18:11:17 +0100

    Make a dumb initial orientation guess

--- src/relax/relax.cpp
+++ src/relax/relax.cpp
@@ -3,22 +3,57 @@
 #include <opencalibration/distort/distort_keypoints.hpp>
 
 #include <ceres/ceres.h>
+#include <spdlog/spdlog.h>
 
 #include <unordered_set>
 
 namespace opencalibration
 {
 
+static const Eigen::Quaterniond DOWN_ORIENTED_NORTH = Eigen::Quaterniond::Identity(); // TODO FIXME
+
 void initializeOrientation(const std::vector<size_t> &node_ids, MeasurementGraph &graph)
 {
+    std::vector<std::pair<double, Eigen::Quaterniond>> hypotheses;
     for (size_t node_id : node_ids)
     {
-        MeasurementGraph::Node *node1 = graph.getNode(node_id);
-        if (node1 == nullptr)
+        MeasurementGraph::Node *node = graph.getNode(node_id);
+        if (node == nullptr)
             return;
 
-        // TODO: get connections, use relative orientations from RANSAC to initialize orientations
-        node1->payload.orientation = Eigen::Quaterniond::Identity();
+        // get connections
+        hypotheses.clear();
+        hypotheses.reserve(node->getEdges().size());
+
+        for (size_t edge_id : node->getEdges())
+        {
+            MeasurementGraph::Edge *edge = graph.getEdge(edge_id);
+            size_t other_node_id = edge->getDest() == node_id ? edge->getSource() : edge->getDest();
+            MeasurementGraph::Node *other_node = graph.getNode(other_node_id);
+
+            Eigen::Quaterniond transform = edge->getDest() == node_id ? edge->payload.relative_rotation.inverse()
+                                                                      : edge->payload.relative_rotation;
+
+            // TODO: use relative positions as well when NaN
+            bool other_ori_nan = other_node->payload.orientation.coeffs().hasNaN();
+            Eigen::Quaterniond other_orientation =
+                other_ori_nan ? DOWN_ORIENTED_NORTH : other_node->payload.orientation;
+            double weight = other_ori_nan ? 0.1 : 1;
+            Eigen::Quaterniond hypothesis = transform * other_orientation;
+
+            hypotheses.emplace_back(weight, hypothesis);
+        }
+
+        // for now just make a dumb weighted average
+        double weight_sum = 0;
+        Eigen::Vector4d vec_sum;
+        for (const auto &h : hypotheses)
+        {
+            weight_sum += h.first;
+            vec_sum += h.first * h.second.coeffs();
+        }
+
+        node->payload.orientation.coeffs() = weight_sum > 0 ? vec_sum / weight_sum : DOWN_ORIENTED_NORTH.coeffs();
     }
 }
 
