3c80883a021e0c15844008550a60be5ca12dbcbc
Julian Kent <jkflying@gmail.com>
2023-04-18 19:45:51 +0200

    Add build-essential

--- tools/install_dependencies.sh
+++ tools/install_dependencies.sh
@@ -3,4 +3,4 @@
 set -ex
 sudo apt update || (apt-get update && apt-get install sudo)
 sudo apt upgrade -y
-DEBIAN_FRONTEND=noninteractive sudo apt install -y ccache ninja-build libeigen3-dev libopencv-dev libgdal-dev libceres-dev libgtest-dev rapidjson-dev libspdlog-dev libtinyxml2-dev clang-format-11 lcov
+DEBIAN_FRONTEND=noninteractive sudo apt install -y cmake ccache ninja-build libeigen3-dev libopencv-dev libgdal-dev libceres-dev libgtest-dev rapidjson-dev libspdlog-dev libtinyxml2-dev clang-format-11 lcov g++-11 build-essential
