da0d6c7fe09d9546076a11efb68b4fc8e77ebb64
Julian Kent <jkflying@gmail.com>
2023-04-18 19:24:18 +0200

    Don't explicitly install g++-11

--- tools/install_dependencies.sh
+++ tools/install_dependencies.sh
@@ -2,4 +2,4 @@
 
 set -ex
 sudo apt-get update
-sudo apt-get install ccache ninja-build libeigen3-dev libopencv-dev libgdal-dev libceres-dev libgtest-dev rapidjson-dev libspdlog-dev libtinyxml2-dev clang-format-11 g++-11 lcov
+sudo apt-get install ccache ninja-build libeigen3-dev libopencv-dev libgdal-dev libceres-dev libgtest-dev rapidjson-dev libspdlog-dev libtinyxml2-dev clang-format-11 lcov
