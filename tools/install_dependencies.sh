af0da625d0e030346ef1613519c6454a41f407cc
Julian Kent <jkflying@gmail.com>
2023-04-18 20:09:28 +0200

    Fix libunwind

--- tools/install_dependencies.sh
+++ tools/install_dependencies.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
 set -ex
-sudo apt update || (apt-get update && apt-get install sudo)
-sudo apt upgrade -y
-sudo apt install -y cmake ccache ninja-build libeigen3-dev libopencv-imgcodecs-dev libgdal-dev libceres-dev libgtest-dev rapidjson-dev libspdlog-dev libtinyxml2-dev clang-format-11 lcov g++-11 build-essential
+sudo apt-get update || (apt-get update && apt-get install sudo)
+sudo apt-get install -y cmake ccache ninja-build libeigen3-dev libopencv-imgcodecs-dev libgdal-dev libceres-dev libgtest-dev rapidjson-dev libspdlog-dev libtinyxml2-dev clang-format-11 lcov g++-11 build-essential libunwind-dev
