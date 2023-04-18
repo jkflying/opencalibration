40c1c78ffb164675693d38f2961ed5e0d727aca8
Julian Kent <jkflying@gmail.com>
2023-04-18 19:20:16 +0200

    Install dependencies as part of script

---
+++ tools/install_dependencies.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+set -ex
+sudo apt-get update
+sudo apt-get install ccache ninja-build libeigen3-dev libopencv-dev libgdal-dev libceres-dev libgtest-dev rapidjson-dev libspdlog-dev libtinyxml2-dev clang-format-11 g++-11 lcov
