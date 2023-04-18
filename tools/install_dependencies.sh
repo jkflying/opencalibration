ba1d87aa8c55117323160831e9e2d0d84bb66226
Julian Kent <jkflying@gmail.com>
2023-04-18 21:18:13 +0200

    Custom build opencv

--- tools/install_dependencies.sh
+++ tools/install_dependencies.sh
@@ -2,4 +2,12 @@
 
 set -ex
 sudo apt-get update || (apt-get update && apt-get install sudo)
-sudo apt-get install -y cmake ccache ninja-build libeigen3-dev libopencv-imgcodecs-dev libgdal-dev libceres-dev libgtest-dev rapidjson-dev libspdlog-dev libtinyxml2-dev clang-format-11 lcov g++-11 build-essential libunwind-dev
+sudo apt-get install -y cmake ccache ninja-build libeigen3-dev libgdal-dev libceres-dev libgtest-dev rapidjson-dev libspdlog-dev libtinyxml2-dev clang-format-11 lcov g++-11 build-essential libunwind-dev git
+
+rm -rf external/install/
+
+mkdir -p external/build/opencv
+rm -rf external/build/opencv/* || echo "No files found"
+pushd external/build/opencv/
+cmake ../../opencv -G Ninja -DBUILD_TESTS=OFF -DBUILD_opencv_apps=OFF -DBUILD_opencv_dnn=OFF -DBUILD_opencv_gapi=OFF -DBUILD_opencv_highgui=OFF -DBUILD_opencv_js_bindings_generator=OFF -DBUILD_opencv_ml=OFF -DBUILD_opencv_objdetect=OFF -DBUILD_opencv_photo=OFF -DBUILD_opencv_python_bindings_generator=OFF -DBUILD_opencv_stitching=OFF -DBUILD_opencv_ts=OFF -DBUILD_opencv_video=OFF -DBUILD_opencv_videoio=OFF  -DCMAKE_INSTALL_PREFIX=`pwd`/../../install && ninja && ninja install
+popd
