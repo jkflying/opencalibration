2b150b29ecf56d751487bbc42b77ef746d01d942
Julian Kent <jkflying@gmail.com>
2023-04-18 19:41:12 +0200

    Tweaks to install script

--- tools/install_dependencies.sh
+++ tools/install_dependencies.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 set -ex
-sudo apt-get update
-sudo apt-get install ccache ninja-build libeigen3-dev libopencv-dev libgdal-dev libceres-dev libgtest-dev rapidjson-dev libspdlog-dev libtinyxml2-dev clang-format-11 lcov
+sudo apt update || (apt-get update && apt-get install sudo)
+sudo apt upgrade -y
+DEBIAN_FRONTEND=noninteractive sudo apt install -y ccache ninja-build libeigen3-dev libopencv-dev libgdal-dev libceres-dev libgtest-dev rapidjson-dev libspdlog-dev libtinyxml2-dev clang-format-11 lcov
