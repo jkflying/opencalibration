a97aabb1182091402be58f5e69f2475b731c9c12
Julian Kent <jkflying@gmail.com>
2026-09-14 19:05:31 +0200

    Fix style

--- tools/fix_style.sh
+++ tools/fix_style.sh
@@ -8,9 +8,9 @@ DIRS="${@:-$DEFAULT_DIRS}"
 for arg in $DIRS
 do
     if [ -f $arg ]; then
-        clang-format-14 -i -style='{BasedOnStyle: Microsoft}' $arg
+        clang-format-18 -i -style='{BasedOnStyle: Microsoft}' $arg
     elif [ -d $arg ]; then
-        find $arg -iname '*.h' -o -iname '*.cpp' -o -iname '*.hpp' | xargs clang-format-14 -i -style='{BasedOnStyle: Microsoft}'
+        find $arg -iname '*.h' -o -iname '*.cpp' -o -iname '*.hpp' | xargs clang-format-18 -i -style='{BasedOnStyle: Microsoft}'
         find $arg -iname '*.h' -o -iname '*.cpp' -o -iname '*.hpp' | xargs chmod 644
     fi
 done
